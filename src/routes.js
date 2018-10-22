998a19d7e265e93451bad2851b2e5332192858ba
Roman <romansidorenko1990@gmail.com>
2018-10-22 11:55:51 +0300

    merge

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 
-import Home from './components/Home/home';пш
+import Home from './components/Home/home';
 
 class Routes extends Component {
   render() {
